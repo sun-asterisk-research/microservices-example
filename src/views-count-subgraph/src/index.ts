96ad22333287a71355ddbbf6e323105924b2d698
Son Dao Thai <dtson96@gmail.com>
2023-07-28 15:38:48 +0700

    feat: send trace to Jaeger for Rust, Typescript service

--- src/views-count-subgraph/src/index.ts
+++ src/views-count-subgraph/src/index.ts
@@ -2,6 +2,10 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { resolvers } from './resolvers';
 import { typeDefs } from './schema';
+import { tracingSdk } from './tracing';
+
+// Start tracing sdk
+tracingSdk.start()
 
 export interface ServerContext {
   /** The hash code from User-Agent, Real-Client-IP, Location... */
