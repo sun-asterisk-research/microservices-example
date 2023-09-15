73ea445ff062014d7ccb2707c5a449bdbf4c7d32
Nguyen Huu Kim <13513658+kimyvgy@users.noreply.github.com>
2023-09-15 21:43:05 +0700

    refactor: Config devspace for hasura stack

--- src/web-app/devspace_start.sh
+++ src/web-app/devspace_start.sh
@@ -1,5 +1,16 @@
-#!/bin/sh
-set +e  # Continue on errors
+#!/bin/bash
+set +e # Continue on errors
+
+export NODE_ENV=development
+if [ -f "yarn.lock" ]; then
+  echo "Installing Yarn Dependencies"
+  yarn
+else
+  if [ -f "package.json" ]; then
+    echo "Installing NPM Dependencies"
+    npm install
+  fi
+fi
 
 COLOR_BLUE="\033[0;94m"
 COLOR_GREEN="\033[0;92m"
@@ -22,19 +33,11 @@ Welcome to your development container!
 This is how you can work with it:
 - Files will be synchronized between your local machine and this container
 - Some ports will be forwarded, so you can access this container via localhost
-- Run \`${COLOR_GREEN}go run main.go${COLOR_RESET}\` to start the application
+- Run \`${COLOR_GREEN}yarn dev${COLOR_RESET}\` to start the application
 "
 
-# Set terminal prompt
-export PS1="\[${COLOR_BLUE}\]devspace\[${COLOR_RESET}\] ./\W \[${COLOR_BLUE}\]\\$\[${COLOR_RESET}\] "
-if [ -z "$BASH" ]; then export PS1="$ "; fi
-
-# Include project's bin/ folder in PATH
-export PATH="./bin:$PATH"
-
-# Open shell
-bash --norc
-
-# Start app
-yarn install --production=false --frozen-lockfile
-yarn dev
+# 1. Set terminal prompt
+# 2. Include project's bin/ folder in PATH
+echo 'export PS1="\[${COLOR_BLUE}\]devspace\[${COLOR_RESET}\] ./\W \[${COLOR_BLUE}\]\\$\[${COLOR_RESET}\] "' >~/.bashrc
+echo 'if [ -z "$BASH" ]; then export PS1="$ "; fi' >>~/.bashrc
+echo 'export PATH="./bin:$PATH"' >>~/.bashrc
