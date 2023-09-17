a69dd77034ba5563a9fba41a5cfbb3dcb77953b6
Nguyen Huu Kim <13513658+kimyvgy@users.noreply.github.com>
2023-09-17 17:10:15 +0700

    refactor: Config devspace for content-views-subgraph

--- src/web-app/devspace_start.sh
+++ src/web-app/devspace_start.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set +e # Continue on errors
 
-ls -la
-
-export NODE_ENV=development
-if [ -f "yarn.lock" ]; then
-  echo "Installing Yarn Dependencies"
-  yarn
-else
-  if [ -f "package.json" ]; then
-    echo "Installing NPM Dependencies"
-    npm install
-  fi
-fi
-
 COLOR_BLUE="\033[0;94m"
 COLOR_GREEN="\033[0;92m"
 COLOR_RESET="\033[0m"
@@ -24,4 +11,8 @@ echo "export PS1=\"\[${COLOR_BLUE}\]devspace\[${COLOR_RESET}\] ./\W \[${COLOR_BL
 echo 'if [ -z "$BASH" ]; then export PS1="$ "; fi' >>~/.bashrc
 echo 'export PATH="./bin:$PATH"' >>~/.bashrc
 
+echo "Installing Yarn Dependencies"
+yarn install
+
+echo "Starting application..."
 yarn dev
