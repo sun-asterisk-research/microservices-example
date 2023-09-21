eabdbe2b15ba80cc48a15e262ca738f1d6b8dbce
Nguyen Huu Kim <13513658+kimyvgy@users.noreply.github.com>
2023-09-21 09:25:19 +0700

    fix: contents-view-subgraph error

--- src/contents-view-subgraph/src/markdown.ts
+++ src/contents-view-subgraph/src/markdown.ts
@@ -1,10 +1,15 @@
 import { createRenderer } from "viblo-sdk/markdown";
 
 export const markdown = createRenderer({
-  baseURL: 'https://viblo.asia',
+  baseURL: "https://viblo.asia",
   katex: {
     maxSize: 500,
     maxExpand: 100,
     maxCharacter: 1000,
+  },
+  // TODO: Why need clipboard???
+  clipboard: {
+    successTextDelay: 2000,
+    maxStringLengthShortcut: 300,
   },
 });
